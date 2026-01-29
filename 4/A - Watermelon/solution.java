340c585acbb349f34e14f14e5a4d5459b786ade9
Trivikram Varma <232066309+trivikram17@users.noreply.github.com>
2026-01-29 11:57:31 +0530

    Add Watermelon [A] from Codeforces

--- 4/A - Watermelon/solution.java
+++ 4/A - Watermelon/solution.java
@@ -1,1 +1,1 @@
-import java.util.*;public class main{public static void main(String [] args){    Scanner sc= new Scanner(System.in);    int x=sc.nextInt();    if(x%2==0 && x>2){        System.out.println("YES");    }else{        System.out.println("NO");    }}}
+import java.util.*;public class main{public static void main(String [] args){    Scanner sc= new Scanner(System.in);    int y=sc.nextInt();    if(y%2==0 && y>2){        System.out.println("YES");    }else{        System.out.println("NO");    }}}
